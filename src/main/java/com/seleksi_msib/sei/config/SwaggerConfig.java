61fa9b028a1062ed0d1cd9a6e9c66e10b981a668
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2024-08-17 23:32:25 +0700

    feat: change desc swagger ui

--- src/main/java/com/seleksi_msib/sei/config/SwaggerConfig.java
+++ src/main/java/com/seleksi_msib/sei/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.seleksi_msib.sei.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Proyek REST API")
                         .version("1.0.0")
-                        .description("REST API untuk manajemen proyek dan lokasinya. \n Create by Muhammad Thoriq Ramadhan"));
+                        .description("REST API untuk manajemen proyek dan lokasinya.")
+                        .contact(new Contact().name("Muhammad Thoriq Ramadhan").email("thoriqramadhanm@gmail.com")));
     }
 }
